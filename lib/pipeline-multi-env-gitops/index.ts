f091983c25483aba7d101b76733ab6059cd53870
bamounjirou <39884076+bamounjirou@users.noreply.github.com>
2022-08-02 21:46:28 -0400

    Update index.ts
    to point to personal repo

--- lib/pipeline-multi-env-gitops/index.ts
+++ lib/pipeline-multi-env-gitops/index.ts
@@ -124,9 +124,9 @@ export default class PipelineMultiEnvGitops {
             );
 
         // Argo configuration per environment
-        const devArgoAddonConfig = createArgoAddonConfig('dev', 'git@github.com:aws-samples/eks-blueprints-workloads.git');
-        const testArgoAddonConfig = createArgoAddonConfig('test', 'git@github.com:aws-samples/eks-blueprints-workloads.git');
-        const prodArgoAddonConfig = createArgoAddonConfig('prod', 'git@github.com:aws-samples/eks-blueprints-workloads.git');
+        const devArgoAddonConfig = createArgoAddonConfig('dev', 'git@github.com:bamounjirou/eks-blueprints-workloads.git');
+        const testArgoAddonConfig = createArgoAddonConfig('test', 'git@github.com:bamounjirou/eks-blueprints-workloads.git');
+        const prodArgoAddonConfig = createArgoAddonConfig('prod', 'git@github.com:bamounjirou/eks-blueprints-workloads.git');
 
         try {
 
@@ -230,7 +230,7 @@ function createArgoAddonConfig(environment: string, repoUrl: string): blueprints
                 }],
                 sourceRepos: [
                     `git@github.com:${element.githubOrg}/${element.githubRepository}.git`,
-                    `git@github.com:aws-samples/eks-blueprints-workloads.git`,
+                    `git@github.com:bamounjirou/eks-blueprints-workloads.git`,
                 ],
             }
         );
